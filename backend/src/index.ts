e3104cbf141bfbad3caac5a275409a47aff1801c
krrakshit <rkrkr365@gmail.com>
2025-01-30 02:12:54 +0530

    prisma init

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,33 @@
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.post("/api/v1/signup",(c)=>{
+  const prisma = new PrismaClient({
+    datasourceUrl: env.DATABASE_URL,
+   }).$extends(withAccelerate())
+  return c.text("signup route")
 })
+
+app.post("/api/v1/signin",(c)=>{
+  return c.text("signin route")
+})
+
+app.get("/api/v1/blog/:id" ,(c)=>{
+  const id = c.req.param('id')
+  console.log(id)
+  return c.text('get blog route')
+})
+
+app.post("/api/v1/signin",(c)=>{
+  return c.text("signin route")
+}) 
+
+app.put("/api/v1/sigin",(c)=>{
+  return c.text("signin route")
+})
+
 
 export default app
